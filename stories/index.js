cae4998d9a71261c03fffff1ff60b050f3ec1cbf
ethanloewen <ethanloewen@gmail.com>
2022-03-23 21:54:25 -0700

    Make Header component

--- stories/index.js
+++ stories/index.js
@@ -10,6 +10,8 @@ import DayListItem from "components/DayListItem.js";
 import DayList from "components/DayList.js";
 import InterviewerListItem from "components/InterviewerListItem.js";
 import InterviewerList from "components/InterviewerList.js";
+import Appointment from "components/Appointment/index.js";
+import Header from "components/Appointment/Header.js";
 
 storiesOf("Button", module)
   .addParameters({
@@ -137,3 +139,18 @@ storiesOf("InterviewerList", module)
       onChange={action("setInterviewer")}
     />
   ));
+
+//-----------------------
+// Appointment component
+//-----------------------
+storiesOf("Appointment", module)
+  .addParameters({
+    backgrounds: [{ name: "white", value: "#fff", default: true }]
+  })
+  .add("Appointment", () => <Appointment />)
+  .add("Appointment with Time", () => (
+    <Appointment
+      time="12pm"
+    />
+  ))
+  .add("Header", () => <Header time="12pm"/>);
