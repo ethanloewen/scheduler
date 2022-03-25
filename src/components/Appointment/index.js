61dbcfbd5a78a9758250bd7c14f018e058711ae6
ethanloewen <ethanloewen@gmail.com>
2022-03-24 17:01:28 -0700

    Change days storage to state

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -6,8 +6,6 @@ import Empty from './Empty';
 
 export default function Appointment(props) {
 
-  console.log(props.interview);
-
   return (
     <article className="appointment">
       <Header time={props.time} />
