d0fef0a41bca64a6ac9ce7e0ccf0abbb7c6d8f3a
ethanloewen <ethanloewen@gmail.com>
2022-03-27 17:05:13 -0700

    Add transition and back functions

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -2,6 +2,28 @@ import React, { useState } from 'react';
 
 export default function useVisualMode(init) {
   const [mode, setMode] = useState(init);
+  const [history, setHistory] = useState([init]);
 
-  return { mode };
+  const transition = (newMode) => {
+    setMode(newMode);
+
+    const tempHistory = history;
+    tempHistory.push(newMode);
+    setHistory(tempHistory);
+  };
+
+  const back = () => {
+    const tempHistory = history;
+    tempHistory.pop();
+    setHistory(tempHistory);
+    
+    const lastElm = history.length - 1;
+    setMode(history[lastElm]);
+  };
+
+  return { 
+    mode,
+    transition,
+    back
+  };
 }
