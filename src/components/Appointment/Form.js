34a70928c25dfcec0c693f1aee841ceedf2c6a80
ethanloewen <ethanloewen@gmail.com>
2022-03-29 20:15:28 -0700

    Create a test file for Form component

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -27,6 +27,7 @@ export default function Form(props) {
             placeholder="Enter Student Name"
             onChange={(event) => setStudent(event.target.value)}
             value={student}
+            data-testid="student-name-input"
           />
         </form>
         <InterviewerList
