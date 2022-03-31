0ba7248b0f7f6cd5e216e9cf321a93d89b22590b
ethanloewen <ethanloewen@gmail.com>
2022-03-30 18:45:58 -0700

    Add test for navigating to Tuesday

--- cypress/integration/navigation.spec.js
+++ cypress/integration/navigation.spec.js
@@ -3,4 +3,10 @@ describe("Navigation", () => {
   it("should visit root", () => {
     cy.visit("/");
   });
+
+  it("should navigate to Tuesday", () => {
+    cy.visit("/");
+    cy.get("li").contains("Tuesday")
+      .click();
+  });
 });
